6b05c051fb9ae36069f5d81c208881104072dd46
einyalynn <dyanarose@gmail.com>
2012-09-26 21:19:20 +0100

    adding jquery animate to the portfolio carousel. Includes jquery easing plugin.

--- app/assets/javascripts/widgets/portfolio.js
+++ app/assets/javascripts/widgets/portfolio.js
@@ -1,3 +1,4 @@
+//noinspection JSUnusedGlobalSymbols
 $.widget('dlr.portfolio',{
     options:{
         width: 690,
@@ -7,37 +8,36 @@ $.widget('dlr.portfolio',{
     _pageCount: 0,
     _create: function(){
         this._pageCount = this.element.find('li.image').length;
-        this.element.find('.portfolio-container ul').css('width', this._pageCount * this.options.width + this.options.marginLeftOffset);
-        var index = 0;
+        this.element.find('.portfolio-container > ul').css('width', this._pageCount * this.options.width + this.options.marginLeftOffset);
         this._bindEvents();
         var widgetContext = this;
         for(var i = 1; i <= this._pageCount; i++){
-            var pageClass = 'page_' + i.toString();
-            var pagerElement = $('<div class=' + pageClass + '>' + i.toString() + '</div>').click(
+            //var pageClass = 'page_' + i.toString();
+            var pagerElement = $('<div>' + i.toString() + '</div>').click(
                 function(){
-                    widgetContext._setCurrentPage(parseInt($(this).text()));
-                    widgetContext._showCurrentPage();
+                    widgetContext.goToPage(parseInt($(this).text()));
                 });
             this.element.find('.portfolio-pager').append(pagerElement);
         }
         if(this._pageCount > 0){
             this._currentPage = 1;
-            this._showCurrentPage();
+            this._showCurrentPage(0);
         }
 
     },
     next: function(){
         this._incrementCurrentPage(1);
-        this._showCurrentPage();
+        this._showCurrentPage(1000);
     },
     previous: function(){
 
         this._incrementCurrentPage(-1);
-        this._showCurrentPage();
+        this._showCurrentPage(1000);
     },
     goToPage: function(pageNumber){
+        var easingDuration = Math.pow(this._currentPage - pageNumber, 2) === 1 ? 1000 : 2000;
         this._setCurrentPage(pageNumber);
-        this._showCurrentPage();
+        this._showCurrentPage(easingDuration);
     },
     _incrementCurrentPage:function(increment){
         var trialNewPage = this._currentPage + increment;
@@ -52,11 +52,12 @@ $.widget('dlr.portfolio',{
             this._currentPage = trialNewPage;
         }
     },
-    _showCurrentPage: function(){
+    _showCurrentPage: function(duration){
         var index =  this._currentPage - 1;
         var margin = -1 * index * this.options.width;
-        //need to animate the shift of the margin of the ul
-        this.element.find('.portfolio-container ul').css('margin-left', margin);
+        this.element.find('.portfolio-container > ul').animate({
+            marginLeft: [margin, 'easeInOutCubic']
+        }, duration, undefined);
     },
     _bindEvents: function(){
         var widgetContext= this;
